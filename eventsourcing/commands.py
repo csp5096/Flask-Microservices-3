352dc7891784c8afcbbfac2a96f6a5157573e09e
csp5096 <christopherscpowell@gmail.com>
2018-09-26 19:48:15 -0500

    Proceeding to add eventsourcing

---
+++ eventsourcing/commands.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import os
+
+class useregister(object):
+    def __init__(self, user_id, user_name, password, emailid):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.password = password
+        self.emailid = emaild
+
+class updatepassword(object):
+    def __init__(self, user_id, new_password, orginal_version):
+        self.item_id = item_id
+        self.new_password = new_password
+        self.orginal_version = orginal_version
+    
